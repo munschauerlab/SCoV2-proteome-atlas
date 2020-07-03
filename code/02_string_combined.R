bc7cc4f6db83be2a5766259fdc555daddc92aec6
caleblareau <caleblareau@g.harvard.edu>
2020-07-03 09:12:31 -0700

    misc

--- code/02_string_combined.R
+++ code/02_string_combined.R
@@ -1,26 +1,37 @@
 library(data.table)
 library(dplyr)
+library(Matrix)
+library(GGally)
+library(network)
 
 # Import database
 if(!exists("string_v11")){
   source('00_string_v11.R')
 }
 # Import gene names
-all <- fread("../data/all_proteomics.txt") %>% arrange(desc(Log.P.Value.SCoV.over.Mock)) %>%
-  filter(adj.P.Val.SCoV.over.Mock < 0.05 & geneSymbol %ni% viral_genes)
+all <- fread("../data/SCoV2_bulkProteome.txt") %>% arrange(log(P.Value.SCoV.over.Mock)) %>%
+  filter(species == "HOMO SAPIENS") 
+
 top1ms <- all[!duplicated(all$geneSymbol),]
-rap <- fread("../data/MM_proteomics_data_limmaFDR.txt") %>%
-  filter(adj.P.Val.SARS.CoV.over.RMRP < 0.2 & geneSymbol %ni% viral_genes)
+rap <- fread("../data/SCoV2_RAPms.txt") %>%
+  filter(adj.P.Val.SCoV2.over.RMRP < 0.05 & species == "HOMO SAPIENS" & logFC.SCoV2.over.RMRP > 0)
+
+merge_df <- left_join(rap, top1ms, by = c("geneSymbol"))
+
+top1ms <- top1ms %>% 
+  filter(adj.P.Val.SCoV.over.Mock < 0.05)
+
+bulk_down <- top1ms %>% filter(logFC.SCoV.over.Mock < 0) %>% pull(geneSymbol)
 
 # Query
-df <- pull_string_v11(min_score = 700, genes_keep = unique(c(rap$geneSymbol, all$geneSymbol)))
+df <- pull_string_v11(min_score = 700, genes_keep = unique(c(rap$geneSymbol, top1ms$geneSymbol)))
 
 # Reorientate
 df_filt <- df[(df$node1 %in% top1ms$geneSymbol) | (df$node2 %in% top1ms$geneSymbol) ,]
-df_filt$N1inRAP <- df_filt$node1 %in% top1ms$geneSymbol
-df_filt$N2inRAP <- df_filt$node2 %in% top1ms$geneSymbol
+df_filt$N1inRAP <- df_filt$node1 %in% rap$geneSymbol
+df_filt$N2inRAP <- df_filt$node2 %in% rap$geneSymbol
 
-df_filt2 <- df_filt[df_filt$N1inRAP + df_filt$N2inRAP < 2,]
+df_filt2 <- df_filt[(df_filt$N1inRAP + df_filt$N2inRAP) ==1 ,]
 
 #write.table(df_filt2, file = "../output/string/RAP_MS_STRING_network.tsv", 
 #            sep = "\t", quote = FALSE, col.names = TRUE, row.names = FALSE)
@@ -40,16 +51,24 @@ colnames(sm) <- levels
 net <- network(sm)
 
 # Show the full network
+IFN <- c("STAT1","IRF9","STAT2","TYK2","JAK1","IFIT1","IFIT3","IFITM3","OAS2","SG15","BST2","SP110","UBE2L6","ADAR","TGIF1","RXRA","BRD3","IFI30")
 color_vec <- case_when(
-  levels %in% c(rap$geneSymbol) & levels %in% c(all$geneSymbol) ~ "both", 
+  levels %in% c(rap$geneSymbol) & levels %in% c(top1ms$geneSymbol) ~ "both", 
   levels %in% c(rap$geneSymbol) ~ "RAP",
-  TRUE ~ "Bulk"
+  
+  levels %in% bulk_down ~ "BulkDown",
+  TRUE ~ "BulkUp"
 )
+table(color_vec)
 net %v% "color" = color_vec
 
-set.seed(3)
-pAI <- ggnet2(net, label = rap$geneSymbol, color = "color",  legend.position = "bottom", 
-              palette = c("Bulk" = "grey", "both" = "red", "RAP" =  "dodgerblue"), size = 3, label.size = 4)
-cowplot::ggsave2(pAI, file = "../output/string/String_combined_RAP+bulk_27June2020_v11.pdf", width = 8, height = 8)
+i = 17
+lapply(1:25, function(i){
+  set.seed(i)
+  pAI <- ggnet2(net, label = c(rap$geneSymbol), color = "color",  legend.position = "bottom", 
+                alpha = 0.75, size = "degree",
+                palette = c("BulkUp" = "lightgrey", "BulkDown"= "black", "both" = "red", "RAP" =  "dodgerblue", "highlight" = "green"),label.size = 2) +  
+    guides( size = FALSE)
+  cowplot::ggsave2(pAI, file = paste0("../output/string/String_combined_RAP+bulk_2July2020_v11_FDR05_seed",as.character(i),".pdf"), width = 8, height = 8)
 
-
+})
--- code/11_gsea_bulk_proteomics.R
+++ code/11_gsea_bulk_proteomics.R
@@ -4,13 +4,12 @@ library(data.table)
 library(org.Hs.eg.db)
 library(clusterProfiler)
 library(msigdbr)
+library(xlsx)
 
-
-viral_genes <- c("N", "S", "3a", "M", "ORF9b", "PP1AB")
 
 # Import differential gene expression
-all <- fread("../data/all_proteomics.txt") %>% arrange(desc(Log.P.Value.SCoV.over.Mock)) %>%
-  filter(!(geneSymbol %in% viral_genes))
+all <- fread("../data/SCoV2_bulkProteome.txt") %>% arrange(log(P.Value.SCoV.over.Mock)) %>%
+  filter(species == "HOMO SAPIENS") 
 diff_df <- all[!duplicated(all$geneSymbol),]
 
 tl <- bitr(diff_df$gene, fromType="SYMBOL", toType="ENTREZID", OrgDb="org.Hs.eg.db", drop = FALSE) 
@@ -19,29 +18,48 @@ diff_df$gene_id <- name_vec[as.character(diff_df$gene)]
 diff_df <- diff_df[complete.cases(diff_df),]
 
 # Do GSEA
-geneList <- diff_df[["logFC.SCoV.over.Mock"]] * diff_df[["Log.P.Value.SCoV.over.Mock"]]; names(geneList) <- diff_df[["geneSymbol"]]
+geneList <- diff_df[["logFC.SCoV.over.Mock"]] * -1*log10(diff_df[["P.Value.SCoV.over.Mock"]]); names(geneList) <- diff_df[["geneSymbol"]]
 geneList <- sort(geneList, decreasing = FALSE)
 
-h_df = msigdbr(species = "Homo sapiens") %>% dplyr::filter(gs_cat == "C5" & gs_subcat == "BP")
-pathway_list = h_df %>% split(x = .$gene_symbol, f = .$gs_name)
+h_df_hallmark = msigdbr(species = "Homo sapiens") %>% dplyr::filter(gs_cat == "H")
+pathway_list_hallmark = h_df_hallmark %>% split(x = .$gene_symbol, f = .$gs_name)
+names(pathway_list_hallmark) <- gsub("HALLMARK_", "", names(pathway_list_hallmark))
 
-fgseaRes <- fgseaMultilevel(pathways = pathway_list, 
-                            stats    = geneList,
-                            minSize  = 15,
-                            maxSize  = 500)
-
-if(FALSE){
-  collapsedPathways <- collapsePathways(fgseaRes[order(pval)][padj < 0.01], 
-                                        pathway_list, geneList)
-  mainPathways <- fgseaRes[pathway %in% collapsedPathways$mainPathways][
-    order(-abs(NES)), pathway]
-  plotGseaTable(pathway_list[mainPathways], geneList, fgseaRes, 
-                gseaParam = 0.5)
-}
-fgseaRes[,c(1:7)] %>% arrange((padj)) %>% head(100)
+h_df_c5bp = msigdbr(species = "Homo sapiens") %>% dplyr::filter(gs_cat == "C5" & gs_subcat == "BP")
+pathway_list_c5bp = h_df_c5bp %>% split(x = .$gene_symbol, f = .$gs_name)
 
 
-fgseaRes[,c(1:7)] %>% arrange((padj)) %>% filter(pathway %in% c("GO_RESPONSE_TO_TYPE_I_INTERFERON",
-                                                                "GO_REGULATION_OF_MAPK_CASCADE", "GO_POSITIVE_REGULATION_OF_MAP_KINASE_ACTIVITY"))
+fgseaRes_hallmark <- fgseaMultilevel(pathways = pathway_list_hallmark, 
+                                     stats    = geneList,
+                                     minSize  = 15,
+                                     maxSize  = 500)
+
+fgseaRes_hallmark[,c(1:7)] %>% arrange(pval)
+
+fgseaRes_c5bp <- fgseaMultilevel(pathways = pathway_list_c5bp, 
+                                 stats    = geneList,
+                                 minSize  = 15,
+                                 maxSize  = 500)
+
+
+# Visualize the hallmark pathways in figure
+collapsedPathways <- collapsePathways(fgseaRes_hallmark[order(pval)][padj < 0.1], 
+                                      pathway_list_hallmark, geneList)
+mainPathways <- fgseaRes_hallmark[pathway %in% names(collapsedPathways$parentPathways)][
+  order(-abs(NES)*(sign(NES) + 2)), pathway]
+pdf("../output/GSEA_hallmark_viz.pdf", width = 6, height = 8)
+plotGseaTable(pathway_list_hallmark[mainPathways], geneList, fgseaRes_hallmark, 
+              gseaParam = 0.5)
+dev.off()
+
+
+
+# Export pathways
+selected_pathways <- c("GO_RESPONSE_TO_TYPE_I_INTERFERON","GO_REGULATION_OF_MAPK_CASCADE", "GO_POSITIVE_REGULATION_OF_MAP_KINASE_ACTIVITY")
+fgseaRes_c5bp %>% filter(pathway %in% selected_pathways)
+
+fgseaRes_c5bp[,c(1:7)] %>% arrange((padj)) %>% 
+  write.xlsx("../output/TableSX_GO_BP_Proteome_GSEA-30June.xlsx", sheetName = "MSigDB C5 BP", 
+             col.names = TRUE, row.names = FALSE, append = FALSE)
 
 
