da084fe30ba3423ddd17ff072d8dcd3edf008839
Caleb Lareau <clareau@stanford.edu>
2021-04-08 08:32:30 -0700

    update l2 to reflect 100/500/100 convention

--- clipseq_followup/data/metagene_pileup/l2.sh
+++ clipseq_followup/data/metagene_pileup/l2.sh
@@ -1,10 +1,10 @@
-bsub -q big computeMatrix scale-regions -R hg38_merged_5utr.bed -S CNBP_ns.bw -b 0 -a 0 --regionBodyLength 1000 --skipZeros --outFileName CNBP_5utr.tab -o ../output/CNBP_5utr.tsv.gz
-bsub -q big computeMatrix scale-regions -R hg38_merged_5utr.bed -S LARP1_ns.bw -b 0 -a 0 --regionBodyLength 1000 --skipZeros --outFileName LARP1_5utr.tab -o ../output/LARP1_5utr.tsv.gz
+computeMatrix scale-regions -R hg38_merged_5utr.bed -S CNBP_ns.bw -b 0 -a 0 --regionBodyLength 100 --skipZeros --outFileName CNBP_5utr.tab -o ../output/CNBP_5utr.tsv.gz
+computeMatrix scale-regions -R hg38_merged_5utr.bed -S LARP1_ns.bw -b 0 -a 0 --regionBodyLength 100 --skipZeros --outFileName LARP1_5utr.tab -o ../output/LARP1_5utr.tsv.gz
 
-bsub -q big computeMatrix scale-regions -R hg38_merged_cds.bed -S CNBP_ns.bw -b 0 -a 0 --regionBodyLength 5000 --skipZeros --outFileName CNBP_cds.tab -o ../output/CNBP_cds.tsv.gz
-bsub -q big computeMatrix scale-regions -R hg38_merged_cds.bed -S LARP1_ns.bw -b 0 -a 0 --regionBodyLength 5000 --skipZeros --outFileName LARP1_cds.tab -o ../output/LARP1_cds.tsv.gz
+computeMatrix scale-regions -R hg38_merged_cds.bed -S CNBP_ns.bw -b 0 -a 0 --regionBodyLength 500 --skipZeros --outFileName CNBP_cds.tab -o ../output/CNBP_cds.tsv.gz
+computeMatrix scale-regions -R hg38_merged_cds.bed -S LARP1_ns.bw -b 0 -a 0 --regionBodyLength 500 --skipZeros --outFileName LARP1_cds.tab -o ../output/LARP1_cds.tsv.gz
 
-bsub -q big computeMatrix scale-regions -R hg38_merged_3utr.bed -S CNBP_ns.bw -b 0 -a 0 --regionBodyLength 1000 --skipZeros --outFileName CNBP_3utr.tab -o ../output/CNBP_3utr.tsv.gz
-bsub -q big computeMatrix scale-regions -R hg38_merged_3utr.bed -S LARP1_ns.bw -b 0 -a 0 --regionBodyLength 1000 --skipZeros --outFileName LARP1_3utr.tab -o ../output/LARP1_3utr.tsv.gz
+computeMatrix scale-regions -R hg38_merged_3utr.bed -S CNBP_ns.bw -b 0 -a 0 --regionBodyLength 100 --skipZeros --outFileName CNBP_3utr.tab -o ../output/CNBP_3utr.tsv.gz
+computeMatrix scale-regions -R hg38_merged_3utr.bed -S LARP1_ns.bw -b 0 -a 0 --regionBodyLength 100 --skipZeros --outFileName LARP1_3utr.tab -o ../output/LARP1_3utr.tsv.gz
 
 
